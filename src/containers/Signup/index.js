6fc334e06887d05fda35060229ed591f430368bb
Jeffrey Gu <jeffreygu@college.harvard.edu>
2021-01-20 10:45:05 -0500

    outlined signup page, linked login to signup

--- src/containers/Signup/index.js
+++ src/containers/Signup/index.js
@@ -1,6 +1,55 @@
 import React from 'react'
+/*import {
+    FormBox, FormTitle, SmallFormField, BigFormField
+} from './styles'*/
 
-const Home = () => <div>Signup</div>
+import { Container } from 'react-bootstrap'
+import { FormGroup, Input, Button, Row, Col } from 'reactstrap'
 
+// need to figure out how to use bootstrap styling for the form without overriding the navbar styling
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default Home
+const Signup = () => {
+    return (
+        <Container align='center'>
+            <h1>Sign Up!</h1>
+            <Row>
+                <Col>
+                    <FormGroup>
+                        <Input type="firstName" name="firstName" id="examplefirstName" placeholder="First Name" />
+                    </FormGroup>
+                </Col>
+                <Col>
+                    <FormGroup>
+                        <Input type="lastName" name="lastName" id="examplelastName" placeholder="Last Name" />
+                    </FormGroup>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <FormGroup>
+                        <Input type="username" name="username" id="exampleusername" placeholder="Username" />
+                    </FormGroup>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <FormGroup>
+                        <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
+                    </FormGroup>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <FormGroup>
+                        <Input type="password" name="password" id="examplePassword" placeholder="Password" />
+                    </FormGroup>
+                </Col>
+            </Row>
+            <Button>Sign Up!</Button>
+        </Container>   
+
+    )
+}
+
+export default Signup
---
+++ src/containers/Signup/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const FormBox = styled.div`
+  width: 100%;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 400px;
+  margin: auto;
+`
+export const FormTitle = styled.h1`
+  font-family: sans-serif;
+  font-size: 1.5em;
+  color: #ff0000;
+`
+export const SmallFormField = styled.input`
+    width: 50%;
+`
+export const BigFormField = styled.input`
+    width: 100%
+`
