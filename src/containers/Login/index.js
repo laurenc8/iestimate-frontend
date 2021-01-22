8782288b97a6a4fcab17a53bec6727e3e2244a48
Jeffrey Gu <jeffreygu@college.harvard.edu>
2021-01-22 11:06:05 -0500

    removed comment

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -46,25 +46,3 @@ const Login = () => {
 }
 
 export default Login
-
-/*
-  const formFields = ['username', 'password']
-  return (
-    <Container>
-      <h1>Welcome Back!</h1>
-      {formFields.map(e => (
-        <Row>
-          <Col>
-            <FormGroup>
-              <Input type={e} name={e} placeholder={e.charAt(0).toUpperCase() + e.slice(1)} />
-            </FormGroup>
-          </Col>
-        </Row>
-      ))}
-      <FormGroup>
-        <Button>Sign in</Button>
-      </FormGroup>
-      <Redirect href="/signup">Don&apos;t have an account? Sign up here!</Redirect>
-    </Container>
-  )
-*/
