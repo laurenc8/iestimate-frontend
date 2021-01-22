4a751591567dacd489fcc80a7de00ce9eb1f3cf0
Jeffrey Gu <jeffreygu@college.harvard.edu>
2021-01-22 11:02:57 -0500

    added login functionality

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useMutation } from '@apollo/react-hooks'
+import { useHistory } from 'react-router-dom'
 import {
-  Container, Input, Col, Row, Button, FormGroup, Redirect,
+  Container, Input, Button, Row, Col, FormGroup, Redirect,
 } from '../../styles'
+import { LOGIN } from './graphql'
 
 const Login = () => {
+  const history = useHistory()
+  const [email, setEmail] = useState('')
+  const [pass, setPass] = useState('')
+  const [login, { loading, error }] = useMutation(LOGIN, {
+    variables: {
+      email,
+      password: pass,
+    },
+    onCompleted: ({ login: { token } }) => {
+      localStorage.setItem('token', token)
+      history.push('/categories')
+    },
+  })
+  return (
+    <Container>
+      <h1>Welcome Back!</h1>
+      <Row>
+        <Col>
+          <FormGroup>
+            <Input type="email" name="email" onChange={e => setEmail(e.target.value)} value={email} />
+          </FormGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <FormGroup>
+            <Input type="password" name="password" onChange={e => setPass(e.target.value)} value={pass} />
+          </FormGroup>
+        </Col>
+      </Row>
+      <FormGroup>
+        <Button onClick={login}>Sign in</Button>
+      </FormGroup>
+      <Redirect href="/signup">Don&apos;t have an account? Sign up here!</Redirect>
+    </Container>
+  )
+}
+
+export default Login
+
+/*
   const formFields = ['username', 'password']
   return (
     <Container>
@@ -23,6 +67,4 @@ const Login = () => {
       <Redirect href="/signup">Don&apos;t have an account? Sign up here!</Redirect>
     </Container>
   )
-}
-
-export default Login
+*/
